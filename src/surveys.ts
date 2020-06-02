58ffd2dbe4f4b5f65353b0e0130b431da2d9dccc
Anna <anna@ushahidi.com>
2020-06-02 13:05:06 +0200

    using prettier

--- src/surveys.ts
+++ src/surveys.ts
@@ -3,56 +3,60 @@
 import axios from 'axios';
 
 export class Surveys {
-    backendUrl:string;
-    token:string;
+  backendUrl: string;
+  token: string;
 
-    constructor (backendUrl:string, token?:string) {
-        this.backendUrl = backendUrl;
-        this.token = token;
+  constructor(backendUrl: string, token?: string) {
+    this.backendUrl = backendUrl;
+    this.token = token;
+  }
+
+  setToken(token?: string): object {
+    this.token = token;
+    return this;
+  }
+
+  async getSurveys(id?: string): Promise<unknown> {
+    try {
+      const url = id
+        ? `${this.backendUrl}/api/v4/surveys/${id}`
+        : `${this.backendUrl}/api/v4/surveys/`;
+      const config = this.token
+        ? {
+            headers: { Authorization: `Bearer ${this.token}` },
+          }
+        : {};
+      const response = await axios.get(url, config);
+      return response.data.result || response.data.results;
+    } catch (err) {
+      return err;
     }
+  }
 
-    setToken(token?:string):object {
-        this.token = token;
-        return this;
-    }
+  async saveSurvey(survey: { id?: string }): Promise<unknown> {
+    const method = survey.id ? 'put' : 'post';
+    const url = survey.id
+      ? `${this.backendUrl}/api/v4/surveys/${survey.id}`
+      : `${this.backendUrl}/api/v4/surveys/`;
+    const res = await axios({
+      method: method,
+      url: url,
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+      },
+      data: survey,
+    });
+    return res;
+  }
 
-    async getSurveys(id?:string):Promise<unknown> {
-        try {
-            const url = id ? `${this.backendUrl}/api/v4/surveys/${id}` : `${this.backendUrl}/api/v4/surveys/`;
-            const config = this.token ? {
-                headers: {"Authorization": `Bearer ${this.token}`}
-            } : {}
-            const response = await axios.get(url, config);
-            return response.data.result || response.data.results;
-        }
-        catch(err) {
-            return err;
-        }
-    }
-
-
-    async saveSurvey(survey:{id?:string}):Promise<unknown> {
-        const method = survey.id ? 'put' : 'post';
-        const url = survey.id ? `${this.backendUrl}/api/v4/surveys/${survey.id}` : `${this.backendUrl}/api/v4/surveys/`;
-            const res = await axios({
-                method: method,
-                url: url,
-                headers:{
-                    "Authorization": `Bearer ${this.token}`
-                },
-                data: survey
-        });
-        return res;
-    }
-
-    async deleteSurvey(id:string):Promise<unknown> {
-            const res = await axios({
-                method: 'delete',
-                url: `${this.backendUrl}/api/v4/surveys/${id}`,
-                headers:{
-                    "Authorization": `Bearer ${this.token}`
-                }
-        });
-        return res;
-    }
+  async deleteSurvey(id: string): Promise<unknown> {
+    const res = await axios({
+      method: 'delete',
+      url: `${this.backendUrl}/api/v4/surveys/${id}`,
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+      },
+    });
+    return res;
+  }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
-export const getLanguages = async (backendURL:string):Promise<unknown> => {
-    try {
-        const response = await axios.get(`${backendURL}/api/v4/languages`);
-        console.log(response)
-        return response.data;
-    } catch(err) {
-        return err;
-    }
-}
+export const getLanguages = async (backendURL: string): Promise<unknown> => {
+  try {
+    const response = await axios.get(`${backendURL}/api/v4/languages`);
+    return response.data;
+  } catch (err) {
+    return err;
+  }
+};
