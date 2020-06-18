c64401c8d30306b6fadedf0ff957f644378e4006
Anna <anna@ushahidi.com>
2020-06-18 17:59:36 +0200

    Adding posts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-
 /* TODO:
-* - Make client_id and client_secret configurable?
-*/
+ * - Make client_id and client_secret configurable?
+ */
 
-export {Surveys} from './surveys';
-export {getAuthToken} from './auth';
-export {Categories} from './categories';
-export {getLanguages} from './utils';
+export { Surveys } from './surveys';
+export { getAuthToken } from './auth';
+export { Categories } from './categories';
+export { getLanguages } from './utils';
+export { Posts } from './posts';
